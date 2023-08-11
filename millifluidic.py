5ad7c56da620eb8de5186740a04b07513307d865
Michael Chen <machen27@gmail.com>
2023-08-11 14:59:50 -0500

    Test data, initial image processing

--- millifluidic.py
+++ millifluidic.py
@@ -1,20 +1,55 @@
 # Code to process images. Should be run from command line targetting a specific folder
 
-import sys
+import sys, os, re
 from argparse import ArgumentParser
+import numpy as np
+import skimage as sk
+from skimage.io import imread
+from skimage.filters import threshold_otsu
+from skimage.measure import label, regionprops_table
+import matplotlib.pyplot as plt
 
 
-def inputParse():
-    # Eventually want to run this from the command line
-    workingFolder = ""
-    fileExt = ""
-    nameFilter = ""
-    return workingFolder, fileExt, nameFilter
+def scanImages(folderName, fileExt,
+               nameFilter, imageList) -> tuple[list, np.array]:
+    """ Scan through image names in folderName with file
+    extension fileExt and matching pattern nameFilter"""
+    filePat = re.compile(nameFilter)
+    os.chdir(folderName) # move to working directory
+    for item in os.listdir('.'):
+        # Filter on filePat and extension
+        # TODO: Should handle passing no nameFilter
+        if re.match(filePat, item) and item.endswith(fileExt):
+            imageList.append(item)
+            imageProcess(item)
+    return imageList
+
+
+def imageProcess(item) -> dict:
+    image = imread(item, as_gray=True)
+    # TODO: Should decide on correct thresholding algorithm
+    # Images may need multiple thresholds to delineate surface vs bottom
+    thresh = threshold_otsu(image)
+    mask = image > thresh
+    label_img = label(mask)
+    fullProps = sk.measure.regionprops(label_img, intensity_image=image)
+    fig, ax = plt.subplots()
+    # Plotting to show the image,
+    # TODO: Should be a flag to plot
+    # TODO: What do we do with the masked images?
+    # TODO: Need to filter out small objects, consider sk.morphology.white_tophat()
+    ax.imshow(mask, cmap=plt.cm.gray)
+    for props in fullProps:
+        equivCircRad = np.sqrt(props.area)/np.pi
+        y0, x0 = props.centroid
+        circle = plt.Circle((x0,y0), equivCircRad, color='r',alpha=0.3)
+        ax.add_patch(circle)
+    return fullProps
 
 
 def main(args) -> int:
-    # User inputs
-    print(args.folderName, args.fileExt, args.nameFilter)
+    imageList = []
+    scanImages(args.folderName, args.fileExt, args.nameFilter, imageList)
     return 0
 
 
